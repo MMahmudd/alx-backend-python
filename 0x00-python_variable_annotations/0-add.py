9ae6a041609b95e549014f67e04aac686a7cb54d
MMahmudd <shametlab@gmail.com>
2023-12-08 09:09:13 +0000

    update tasks

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module_that_holds a_single_function that_adds two_integers."""
+
+
+def add(a: float, b: float) -> float:
+    """ Add two_integers_together."""
+    return a + b
