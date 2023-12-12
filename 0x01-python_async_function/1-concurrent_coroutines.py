d72f87a345b453fcf6360716f7c3b3a7692f45e4
MMahmudd <shametlab@gmail.com>
2023-12-12 23:14:43 +0000

    update task

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,18 +1,31 @@
-#!/usr/bin/env python3
-"""Has a coroutine that delays a certain amount of time and returns it"""
+# 1-concurrent_coroutines.py
+"""Module with concurrent coroutines"""
 
 import asyncio
-import random
+from typing import List
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """
+    Returns_a_list_of_floats_delayed_no_more_than_max_delay
+    Args:
+        n: The_number_of_times_to_call_wait_random
+        max_delay: The_maximum_delay_to_return
+    Returns:
+        A_list_of_random_floats_between_0_and_max_delay
+    """
+    return await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """
     Returns_a_random_float_between_0_and_max_delay
     Args:
-        max_ddelay: The_maximum_delay_to_return
+        max_delay: The_maximum_delay_to_return
     Returns:
         A random_float_between_0_and_max_delay
     """
     rand = random.uniform(0, max_delay)
     await asyncio.sleep(rand)
     return rand
+
